fbfe52d5a1491d59f6b649b7d75c8d0b9459e4e9
espadara <espadara@pirate.capn.gg>
2025-08-27 20:00:29 +0200

    [ADD] strlcpy + tests

--- srcs/sea_strlcpy.c
+++ srcs/sea_strlcpy.c
@@ -15,46 +15,28 @@
 /*       > _.="                            "=._ <                             */
 /*      (_/                                    \_)                            */
 /*                                                                            */
-/*      Filename: sealib.h                                                    */
+/*      Filename: sea_strlcpy.c                                               */
 /*      By: espadara <espadara@pirate.capn.gg>                                */
-/*      Created: 2025/08/23 15:35:18 by espadara                              */
-/*      Updated: 2025/08/27 19:40:54 by espadara                              */
+/*      Created: 2025/08/27 19:40:18 by espadara                              */
+/*      Updated: 2025/08/27 19:52:19 by espadara                              */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SEALIB_H
-# define SEALIB_H
+#include "sealib.h"
 
-/* INCLUDES */
+size_t	sea_strlcpy(char *dst, const char *src, size_t size)
+{
 
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
-# include <ctype.h>
-# include <string.h>
-/* DEFINES  */
+  size_t source_len = sea_strlen(src);
+  size_t len_to_copy = 0;
 
-
-
-/* FUNCTIONS */
-
-/* BOOLEANS  */
-int	sea_isalpha(int c);
-int	sea_isdigit(int c);
-int	sea_isalnum(int c);
-int	sea_isascii(int c);
-int	sea_isprint(int c);
-
-/* STRINGS */
-size_t	sea_strlen(const char *s);
-char	*sea_strcpy(char *dest, const char *src);
-char	*sea_strncpy(char *dest, const char *src, size_t dsize);
-size_t	sea_strlcpy(char *dst, const char *src, size_t size);
-void	*sea_bzero(void *s, size_t n);
-
-/* MEMORY */
-void	*sea_memset(void *s, int c, size_t n);
-void	*sea_memcpy(void *dest, const void *src, size_t n);
-void	*sea_memmove(void *dest, const void *src, size_t n);
-
-#endif
+  if (size == 0)
+    return (source_len);
+  if (source_len < size)
+    len_to_copy = source_len;
+  else
+    len_to_copy = size - 1;
+  sea_memcpy(dst, src, len_to_copy);
+  dst[len_to_copy] = 0;
+  return (source_len);
+}
--- test.c
+++ test.c
@@ -18,7 +18,7 @@
 /*      Filename: test.c                                                      */
 /*      By: espadara <espadara@pirate.capn.gg>                                */
 /*      Created: 2025/08/23 15:38:40 by espadara                              */
-/*      Updated: 2025/08/27 13:32:22 by espadara                              */
+/*      Updated: 2025/08/27 19:58:46 by espadara                              */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,10 @@
 int main(void)
 {
   printf("CONDUCTING TESTS!!!\n");
+
+
+
   puts("\n---ISALPHA---");
-  /* isalpha */
   printf("1 - isalpha!!!\nCHARS |%c| |%c| |%c|\nREAL: |%d| |%d| |%d|\n\
 SEAL: |%d| |%d| |%d|\n", 'a', '4', 'Z', isalpha('a'), isalpha('4'), \
          isalpha('Z'), sea_isalpha('a'), sea_isalpha('4'),sea_isalpha('Z'));
@@ -38,7 +40,9 @@ SEAL: |%d| |%d| |%d|\n", 'A', '@', 'v', isalpha('A'), isalpha('@'), \
   printf("3 - isalpha!!!\nCHARS |%c| |%c| |%c|\nREAL: |%d| |%d| |%d|\n\
 SEAL: |%d| |%d| |%d|\n", 'x', '0', ')', isalpha('x'), isalpha('0'), \
          isalpha(')'), sea_isalpha('x'), sea_isalpha('0'),sea_isalpha(')'));
-  /* isdigit */
+
+
+
   puts("\n---ISDIGIT---");
   printf("1 - isdigit!!!\nCHARS |%c| |%c| |%c|\nREAL: |%d| |%d| |%d|\n\
 SEAL: |%d| |%d| |%d|\n", 'a', '4', 'Z', isdigit('a'), isdigit('4'), \
@@ -49,7 +53,9 @@ SEAL: |%d| |%d| |%d|\n", 'A', '@', 'v', isdigit('A'), isdigit('@'), \
   printf("3 - isdigit!!!\nCHARS |%c| |%c| |%c|\nREAL: |%d| |%d| |%d|\n\
 SEAL: |%d| |%d| |%d|\n", 'x', '0', ')', isdigit('x'), isdigit('0'), \
          isdigit(')'), sea_isdigit('x'), sea_isdigit('0'),sea_isdigit(')'));
-  /* isalnum */
+
+
+
   puts("\n---ISALNUM---");
   printf("1 - isalnum!!!\nCHARS |%c| |%c| |%c|\nREAL: |%d| |%d| |%d|\n\
 SEAL: |%d| |%d| |%d|\n", 'a', '4', 'Z', isalnum('a'), isalnum('4'), \
@@ -60,7 +66,9 @@ SEAL: |%d| |%d| |%d|\n", 'A', '@', 'v', isalnum('A'), isalnum('@'), \
   printf("3 - isalnum!!!\nCHARS |%c| |%c| |%c|\nREAL: |%d| |%d| |%d|\n\
 SEAL: |%d| |%d| |%d|\n", 'x', '0', ')', isalnum('x'), isalnum('0'), \
          isalnum(')'), sea_isalnum('x'), sea_isalnum('0'),sea_isalnum(')'));
-  /* isascii */
+
+
+
     puts("\n---ISASCII---");
   printf("1 - isascii!!!\nCHARS |%c| |%c| |%c|\nREAL: |%d| |%d| |%d|\n\
 SEAL: |%d| |%d| |%d|\n", 'a', '4', 'Z', isascii('a'), isascii('4'), \
@@ -71,7 +79,9 @@ SEAL: |%d| |%d| |%d|\n", 'A', '@', 'v', isascii('A'), isascii('@'), \
   printf("3 - isascii!!!\nCHARS |%c| |%c| |%c|\nREAL: |%d| |%d| |%d|\n\
 SEAL: |%d| |%d| |%d|\n", 250, '0', ')', isascii(250), isascii('0'), \
          isascii(')'), sea_isascii(250), sea_isascii('0'),sea_isascii(')'));
-  /* isprint */
+
+
+
       puts("\n---ISPRINT---");
   printf("1 - isprint!!!\nCHARS |%c| |%c| |%c|\nREAL: |%d| |%d| |%d|\n\
 SEAL: |%d| |%d| |%d|\n", 'a', '4', 'Z', isprint('a'), isprint('4'), \
@@ -90,6 +100,8 @@ SEAL: |%d| |%d| |%d|\n", 250, '0', ')', isprint(250), isprint('0'), \
   char *strlen_test_four = "1234\01234";
   char *strlen_test_five = "xyz";
 
+
+
   puts("\n---STRLEN---");
   printf("1 - strlen ! STRS:\n|%s|\n|%s|\n|%s|\n|%s|\n|%s|\nREAL: |%ld| \
 |%ld| |%ld| |%ld| |%ld|\nSEAL: |%ld| |%ld| |%ld| |%ld| |%ld|\n", \
@@ -98,6 +110,8 @@ SEAL: |%d| |%d| |%d|\n", 250, '0', ')', isprint(250), isprint('0'), \
          strlen(strlen_test_three),strlen(strlen_test_four),strlen(strlen_test_five),\
          sea_strlen(strlen_test_one),sea_strlen(strlen_test_two),sea_strlen(strlen_test_three), \
          sea_strlen(strlen_test_four), sea_strlen(strlen_test_five));
+
+
 
   puts("\n---MEMSET---");
   char memset_test_real_one[] = "hey boy";
@@ -109,6 +123,8 @@ SEAL: |%d| |%d| |%d|\n", 250, '0', ')', isprint(250), isprint('0'), \
   memset(&memset_test_seal_one, '0', 2);
   printf("SEAL STR AFTER : |%s|\n", memset_test_seal_one);
 
+
+
   puts("\n---BZERO---");
   char bzero_real_one[]= "YO I AM STRING";
   char bzero_seal_one[]= "YO I AM STRING";
@@ -119,6 +135,8 @@ SEAL: |%d| |%d| |%d|\n", 250, '0', ')', isprint(250), isprint('0'), \
   bzero(&bzero_seal_one, sizeof(bzero_seal_one));
   printf("SEAL STR AFTER : |%s|\n", bzero_seal_one);
 
+
+
   puts("\n---MEMCPY---");
   char memcpy_real_dest[] = "00000000";
   char memcpy_real_src[] = "Ayo wtf dawg";
@@ -126,6 +144,8 @@ SEAL: |%d| |%d| |%d|\n", 250, '0', ')', isprint(250), isprint('0'), \
   char memcpy_seal_src[] = "Ayo wtf dawg";
   printf("REAL MEMCPY RESULT: |%s| \n", memcpy(memcpy_real_dest, memcpy_real_src, 7));
   printf("SEAL MEMCPY RESULT: |%s| \n", sea_memcpy(memcpy_seal_dest, memcpy_seal_src, 7));
+
+
 
   puts("\n---MEMMOVE---");
    char memmove_real_buffer[] = "abcdefghij";
@@ -138,6 +158,8 @@ SEAL: |%d| |%d| |%d|\n", 250, '0', ')', isprint(250), isprint('0'), \
    sea_memmove(memmove_seal_buffer + 2, memmove_seal_buffer, 5);
    printf("SEAL MEMMOVE RESULT:  |%s|\n", memmove_seal_buffer);
 
+
+
    puts("\n---STRCPY---");
    const char *strcpy_src = "Hello, world!";
    char strcpy_real_dest[] = "XXXXXXXXXXXXXXXXXXX";
@@ -149,6 +171,8 @@ SEAL: |%d| |%d| |%d|\n", 250, '0', ')', isprint(250), isprint('0'), \
    printf("REAL STRCPY RESULT: |%s|\n", strcpy_real_dest);
    sea_strcpy(strcpy_seal_dest, strcpy_src);
    printf("SEAL STRCPY RESULT: |%s|\n", strcpy_seal_dest);
+
+
 
    puts("\n---STRNCPY---");
    printf("--- STRNCPY Test 1: n < src length ---\n");
@@ -184,6 +208,70 @@ SEAL: |%d| |%d| |%d|\n", 250, '0', ')', isprint(250), isprint('0'), \
     printf("REAL STRNCPY RESULT:  |%s|\n", strncpy_real_dest3);
     printf("SEAL STRNCPY RESULT:  |%s|\n", strncpy_seal_dest3);
     printf("(Note: Also not null-terminated)\n");
+
+
+
+    puts("\n---STRLCPY---");
+    size_t real_ret_val;
+    size_t seal_ret_val;
+    printf("--- STRLCPY Test 1: size < src length (Truncation) ---\n");
+    const char *strlcpy_src1 = "This is a long string"; // Length is 21
+    char strlcpy_real_dest1[25] = "XXXXXXXXXXXXXXXXXXXXXXXX";
+    char strlcpy_seal_dest1[25] = "XXXXXXXXXXXXXXXXXXXXXXXX";
+
+    printf("Original REAL dest: |%s|\n", strlcpy_real_dest1);
+    printf("Original SEAL dest: |%s|\n\n", strlcpy_seal_dest1);
+    // Copy into a buffer of size 10. Should copy 9 chars + '\0'.
+    real_ret_val = strlcpy(strlcpy_real_dest1, strlcpy_src1, 10);
+    seal_ret_val = sea_strlcpy(strlcpy_seal_dest1, strlcpy_src1, 10);
+    printf("REAL STRLCPY RESULT:  |%s| (return value: %zu)\n", strlcpy_real_dest1, real_ret_val);
+    printf("SEAL STRLCPY RESULT:  |%s| (return value: %zu)\n", strlcpy_seal_dest1, seal_ret_val);
+    printf("(Note: String is correctly truncated and null-terminated)\n");
+    /*
+     * =========================================================================
+     * Test 2: size is LARGER than the source string (Full Copy)
+     *
+     * BEHAVIOR: Copies the entire string with its null terminator.
+     * It does NOT pad the destination with extra nulls like strncpy.
+     * =========================================================================
+     */
+    printf("\n--- STRLCPY Test 2: size > src length (Full Copy) ---\n");
+    const char *strlcpy_src2 = "short"; // Length is 5
+    char strlcpy_real_dest2[25] = "XXXXXXXXXXXXXXXXXXXXXXXX";
+    char strlcpy_seal_dest2[25] = "XXXXXXXXXXXXXXXXXXXXXXXX";
+
+    printf("Original REAL dest: |%s|\n", strlcpy_real_dest2);
+    printf("Original SEAL dest: |%s|\n\n", strlcpy_seal_dest2);
+    // Copy into a buffer of size 15.
+    real_ret_val = strlcpy(strlcpy_real_dest2, strlcpy_src2, 15);
+    seal_ret_val = sea_strlcpy(strlcpy_seal_dest2, strlcpy_src2, 15);
+    printf("REAL STRLCPY RESULT:  |%s| (return value: %zu)\n", strlcpy_real_dest2, real_ret_val);
+    printf("SEAL STRLCPY RESULT:  |%s| (return value: %zu)\n", strlcpy_seal_dest2, seal_ret_val);
+    printf("(Note: The full string is copied, with no extra padding)\n");
+    /*
+     * =========================================================================
+     * Test 3: size is EXACTLY enough for the string (Edge Case)
+     *
+     * BEHAVIOR: If size = strlen(src) + 1, it's a perfect fit.
+     * If size = strlen(src), it must truncate to size - 1.
+     * =========================================================================
+     */
+    printf("\n--- STRLCPY Test 3: size == src length (Edge Case Truncation) ---\n");
+    const char *strlcpy_src3 = "EdgeCase"; // Length is 8
+    char strlcpy_real_dest3[25] = "XXXXXXXXXXXXXXXXXXXXXXXX";
+    char strlcpy_seal_dest3[25] = "XXXXXXXXXXXXXXXXXXXXXXXX";
+
+    printf("Original REAL dest: |%s|\n", strlcpy_real_dest3);
+    printf("Original SEAL dest: |%s|\n\n", strlcpy_seal_dest3);
+    real_ret_val = strlcpy(strlcpy_real_dest3, strlcpy_src3, 8);
+    seal_ret_val = sea_strlcpy(strlcpy_seal_dest3, strlcpy_src3, 8);
+
+    printf("REAL STRLCPY RESULT:  |%s| (return value: %zu)\n", strlcpy_real_dest3, real_ret_val);
+    printf("SEAL STRLCPY RESULT:  |%s| (return value: %zu)\n", strlcpy_seal_dest3, seal_ret_val);
+    printf("(Note: Correctly truncates to fit the required null terminator)\n");
+
+
+
 
   puts("Done!");
   return (0);
